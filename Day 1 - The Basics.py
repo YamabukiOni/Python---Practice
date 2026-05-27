0d75380d3e4d86b72caf1f38069b6ee10f53e429
YamabukiOni <jaydenpancarci8@gmail.com>
2026-05-27 13:09:12 +1000

    Add files via upload

---
+++ Day 1 - The Basics.py
@@ -0,0 +1,30 @@
+print ('Day 1 - Python Coding')
+print ('Hello, World!')
+
+print (5 + 4) #Addition
+print (3 - 1) #Subtraction
+print (5 * 4) #Multiplucation
+print (20 / 2) #Divison
+print (7 + 5 * 2) #BODMAS
+print (3 ** 2) #Exponential
+print (5 % 4) #Modulo
+print (8 // 3) #Floor Division
+
+#DATA TYPES
+print (type(43)) #Integer
+print (type(3.15)) #Float (decimal)
+print (type(3 + 4j)) #Complex
+print (type('I have the high ground')) #String (text)
+
+print (type(True)) 
+print(type(False)) #BooLean (True of False)
+
+#Variables
+name = 'Yoda'
+print (name) #Console displays Yoda
+
+age = 900
+print (age) #Console displays 900
+
+is_jedi = True
+print (is_jedi) #Console displays True
